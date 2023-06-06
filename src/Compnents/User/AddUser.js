d0c4fff6bc981fc69a17e3c006b4adf9cece93ea
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-06-06 03:03:39 -0700

    addUser.modules.css

--- src/Compnents/User/AddUser.js
+++ src/Compnents/User/AddUser.js
@@ -1,19 +1,20 @@
 import React from "react";
-import Card from "../UI/Card"
-const AddUser =()=>{
-    const onAddSubmitHandler=(event)=>{
-     event.preventDefault();
-    }
-    return(
-     <Card>
-        <form onSubmit={onAddSubmitHandler}>
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+const AddUser = () => {
+  const onAddSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input} >
+      <form onSubmit={onAddSubmitHandler}>
         <label htmlFor="userName">Enter your userName</label>
-            <input className="userName" type="text"/> 
-            <label htmlFor="userAge">Enter your age</label>
-            <input className="userAge" type="number"/>
-            <button type="submit">Add user</button> 
-        </form>
-     </Card>
-    )
-}
+        <input className="userName" type="text" />
+        <label htmlFor="userAge">Enter your age</label>
+        <input className="userAge" type="number" />
+        <button type="submit">Add user</button>
+      </form>
+    </Card>
+  );
+};
 export default AddUser;
