ddc1e6f69051de7241bb0b72697ee79297846335
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-06-06 04:10:51 -0700

    create button.js

--- src/Compnents/User/AddUser.js
+++ src/Compnents/User/AddUser.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 const AddUser = () => {
+const [userName,setUserName]=useState()
+const [userAge,setUserAge]=useState()
+
   const onAddSubmitHandler = (event) => {
     event.preventDefault();
+    setUserAge("");
+    setUserName("");
+    console.log(userName,userAge)
   };
+  const onUserName=(event)=>{
+setUserName(event.target.value)
+  }
+  const onUserAge=(event)=>{
+setUserAge(event.target.value)
+
+  }
   return (
     <Card className={classes.input} >
       <form onSubmit={onAddSubmitHandler}>
         <label htmlFor="userName">Enter your userName</label>
-        <input className="userName" type="text" />
+        <input className="userName" value={userName} type="text"onChange={onUserName}  />
         <label htmlFor="userAge">Enter your age</label>
-        <input className="userAge" type="number" />
+        <input className="userAge" value={userAge} type="number" onChange={onUserAge}/>
         <button type="submit">Add user</button>
       </form>
     </Card>
