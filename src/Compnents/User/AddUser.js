db74140509cfe94fb6e0bb40e81eb0ad48c92095
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-06-06 09:45:42 -0700

    add some features and add css style components

--- src/Compnents/User/AddUser.js
+++ src/Compnents/User/AddUser.js
@@ -3,19 +3,23 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "./Button";
 const AddUser = () => {
-const [userName,setUserName]=useState()
-const [userAge,setUserAge]=useState()
+const [userName,setUserName]=useState("")
+const [userAge,setUserAge]=useState("")
 
   const onAddSubmitHandler = (event) => {
     event.preventDefault();
     setUserAge("");
     setUserName("");
+    if(userName.trim().length === 0 || userAge.trim().length === 0){
+      return ;
+    }
+    if(userAge >1){console.log("enter a valid number")}
     console.log(userName,userAge)
   };
-  const onUserName=(event)=>{
+  const onNameHandler=(event)=>{
 setUserName(event.target.value)
   }
-  const onUserAge=(event)=>{
+  const onAgeHandler=(event)=>{
 setUserAge(event.target.value)
 
   }
@@ -23,9 +27,9 @@ setUserAge(event.target.value)
     <Card className={classes.input} >
       <form onSubmit={onAddSubmitHandler}>
         <label htmlFor="userName">Enter your userName</label>
-        <input className="userName" value={userName} type="text"onChange={onUserName}  />
+        <input className="userName" value={userName} type="text"onChange={onNameHandler}  />
         <label htmlFor="userAge">Enter your age</label>
-        <input className="userAge" value={userAge} type="number" onChange={onUserAge}/>
+        <input className="userAge" value={userAge} type="number" onChange={onAgeHandler}/>
         <Button type="submit"></Button>
       </form>
     </Card>
